5cd30e6dcb44d76cdcfa1ae45c61dc26332b12c8
Mustorze <mustorze@mustorze.com>
2017-08-26 16:55:02 -0300

    First 26/08/2017

--- resources/views/threads/_list.blade.php
+++ resources/views/threads/_list.blade.php
@@ -42,7 +42,7 @@
         </div>
 
         <div class="panel-footer">
-            {{ $thread->visits()->count() }} Visits. =)
+            {{ $thread->visits()->count() }} Visits.
         </div>
     </div>
 @empty
