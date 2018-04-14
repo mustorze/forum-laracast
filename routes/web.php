1ef0d04068afb9827ca179ac1fb09a7e5be19910
Henrique <henrique@mustorze.com>
2018-04-14 10:51:58 -0300

    Must confirm to create a new thread

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/threads', 'ThreadsController@index')->name('threads');
 
 Route::get('/threads/search', 'SearchController@show')->name('threads.search');
-Route::get('/threads/create', 'ThreadsController@create')->name('threads.create');
+Route::get('/threads/create', 'ThreadsController@create')->name('threads.create')->middleware('must-be-confirmed');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
 Route::patch('/threads/{channel}/{thread}', 'ThreadsController@update');
 Route::post('/threads', 'ThreadsController@store')->name('threads.store')->middleware('must-be-confirmed');
